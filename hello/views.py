d39e9bc91bc5b1b285e70d4783dcae924aa2210b
Baytars <feicuihuangfeng@qq.com>
2022-07-11 11:07:01 +0800

    request handling fix - amend

--- hello/views.py
+++ hello/views.py
@@ -15,7 +15,7 @@ def index(request):
 def read_in_chunks(res_object, chunk_size=1024):
     """Lazy function (generator) to read a file piece by piece.
     Default chunk size: 10k."""
-    for chunk in res.iter_content(chunk_size, decode_unicode=True):
+    for chunk in res_object.iter_content(chunk_size, decode_unicode=True):
         try:
             yield chunk
         except:
@@ -24,7 +24,9 @@ def read_in_chunks(res_object, chunk_size=1024):
 @csrf_exempt
 @require_POST
 def p2(request):
-    with requests.get(json.loads(request.body)['url'], stream = True) as res:
+    url = json.loads(request.body)['url']
+    print(url)
+    with requests.get(url, stream = True) as res:
         return StreamingHttpResponse(read_in_chunks(res))
 
 def db(request):
